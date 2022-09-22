359c63a4f7f6f190e74bfcdab1247a6f04f47ea0
Binyam Sintayehu <73512283+Binyam-maker@users.noreply.github.com>
2022-09-22 14:35:01 +0300

    improved header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
+import {HiOutlineSearch, HiOutlineShoppingCart} from 'react-icons/hi'
 
 
 const bannerUrl = "https://rb.gy/vsvv2o"
+
+const links = [{name:"Products", link: '/products'},{name: "Explore", link: '/explore'}, {name:"Support", link: "/support"}, {name:"Business", link: '/business'}]
 
 const Header: () => JSX.Element= () => {
     const [imageUrl, setImageUrl] = useState("")
@@ -11,13 +15,42 @@ const Header: () => JSX.Element= () => {
         setImageUrl(bannerUrl)
     },[])
     return (
-        <div>
-            <div className="relative w-8 h-10 cursor-pointer opacity-100 hover:opacity-80">
-                <Image src={bannerUrl} layout="fill" objectFit="cover" alt='Header Image'/>
+        <div className='sticky top-0 z-30  flex items-center  w-full  bg-[#E7ECEE] pr-3'> 
+
+            {/* Logo container */}
+            <div className="flex items-center justify-center md:w-1/5 bg-white">
+            <Link href={'/'}> 
+            <div className="relative w-6 h-12 mx-4 cursor-pointer opacity-100 hover:opacity-80 bg-orange-500 ">
+                <Image src={bannerUrl} layout="fill" objectFit="contain" alt='Header Image'/>
             </div>
-            {/* <div><p>hello</p></div> */}
+            </Link>
+            </div>
+
+            {/* Links container */}
+            <ul className='invisible justify-center flex-1 space-x-8 md:flex md:visible gap-4 bg-red-500'>
+                {links.map((item) => <Link href={item.link}>{item.name}</Link>)}
+            </ul>
+
+            {/* Icons container */}
+            <div className='flex justify-center item-center gap-4 md:w-1/5'>
+
+            {/* Search */}
+            <div >
+                <HiOutlineSearch className='headerIcon'/>
+            </div>
             
+            {/* Cart */}
+            <Link href={'/checkout'}>
+            <div className='relative'>
+                <HiOutlineShoppingCart
+                className='headerIcon'/>
+                <span className='absolute -right-1 -top-2 text-xs bg-gradient-to-l from-pink-500 to-purple-500  w-4 h-4 rounded-full text-center  text-white'>3</span>
+            </div>
+                </Link>
+            </div>
+        
         </div>
+           
     )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,15 @@ import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="relative flex min-h-screen flex-col items-center justify-center py-2 bg-red-500 overflow-hidden">
       <Head>
         <title>Apple</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* header component */}
-      <Header />
-      <main className="bg-pink-500">       
+     
+      <main className=" container grid place-items-start bg-lime-500 w-screen h-screen overflow-hidden p-2"> 
+       {/* header component */}
+      <Header />      
       </main>
 
      
